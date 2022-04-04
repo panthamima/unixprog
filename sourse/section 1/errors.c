76592d48a9bc7b292263354699b94755f502b34e
panthamima <nastyadima2003@mail.ru>
2022-04-04 17:15:49 +0700

    added trash folder

--- sourse/section 1/errors.c
+++
@@ -1,8 +0,0 @@
-#include "../libc.h"
-
-int main(int argc, char* argv[]) {
-    fprintf(stderr, "EACESS: %s\n", strerror(EACCES));
-    errno = ENOENT;
-    perror(argv[0]);
-    return 0;
-}
--- sourse/section 1/process_id.c
+++
@@ -1,6 +0,0 @@
-#include "../libc.h"
-
-int main(void) {
-    printf("process have id: [%d]\n", (long)getpid());
-    return 0;
-}
